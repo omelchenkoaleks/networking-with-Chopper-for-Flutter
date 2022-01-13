61bbca33b809a8a9c645b71f59ed92d2c0ef5880
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-13 07:25:57 +0200

    preparing the recipe service

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -1,23 +1,13 @@
-import 'package:http/http.dart';
+// This adds the Chopper package and models.
+import 'package:chopper/chopper.dart';
+import 'recipe_model.dart';
+import 'model_response.dart';
+import 'model_converter.dart';
 
+// Here is where you re-enter API Key and ID.
 const String apiKey = '60ae7309346de8a6372afd401889e5bc';
 const String apiId = 'b925f969';
-const String apiUrl = 'https://api.edamam.com/search';
+const String apiUrl = 'https://api.edamam.com';
 
-class RecipeService {
-  Future<dynamic> getRecipes(String query, int from, int to) async {
-    final recipeData = await getData(
-        '$apiUrl?app_id=$apiId&app_key=$apiKey&q=$query&from=$from&to=$to');
-    return recipeData;
-  }
+// TODO: Add @ChopperApi() here
 
-  Future getData(String url) async {
-    print('Calling uri: $url');
-    final response = await get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      print(response.statusCode);
-    }
-  }
-}
